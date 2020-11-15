a096cf8daac995a917013fb4f6f22f83a5b8f0c4
Keith Medlin <keith@keithmedlin.com>
2020-11-15 12:39:12 -0500

    FIX: Adjusted Script to work with Dropbox

--- getpostimglinks.py
+++ getpostimglinks.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome('C:/Users/Keith/Documents/Python Scripts/chromedriver.exe')
 
 # You'll need to update this each time you run the script for a new folder at postimg.cc
-url = "https://postimg.cc/gallery/D41BF8w"
+url = "YOUR DROPBOX LINK HERE"
 
 # Regex to identify JUST the image files. This will pick up jpg, gif, png, but you can edit to your taste.
 reg = re.compile('(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-]).(?:jpg|gif|png)')
@@ -25,11 +25,11 @@ elem = browser.find_element_by_tag_name("body")
 
 # If you have a massive listing of images, you may need to adjust this so that the page will scroll down enough.
 # This is necessary since the postimg site does progressive loading of images.
-no_of_pagedowns = 20
+no_of_pagedowns = 25
 
 while no_of_pagedowns:
     elem.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
+    time.sleep(0.5)
     no_of_pagedowns-=1
 
 # instantiate the BeautifulSoup object to start parsing the html from postimg
@@ -37,7 +37,7 @@ soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 # This line may need to be updated in the future if postimg.cc changes how they code the page.
 # If you're getting back garbage results...check to ensure this makes sense still using Chrom Dev Tools
-thumbs = soup.find_all("a", class_="img")
+thumbs = soup.find_all("a", class_="sl-link sl-link--file")
 
 # Get the appropriate element
 for thumb in thumbs:
